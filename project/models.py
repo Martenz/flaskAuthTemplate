7727bc068f7920870340d844de1e0bfdf90a8cab
Martenz <ing.martino.boni@gmail.com>
2021-02-25 14:33:35 +0100

    test db migration

--- project/models.py
+++ project/models.py
@@ -6,9 +6,11 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    url_image = db.Column(db.String(250))
 
 class points(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     coordinates = db.Column(db.String(250), nullable=False)
+    url_image = db.Column(db.String(250))
     body = db.Column(db.Text())
