f029118f328b77f3b005db345191076327222704
Martenz <ing.martino.boni@gmail.com>
2021-02-26 03:11:02 +0100

    better template

--- project/form.py
+++ project/form.py
@@ -1,7 +1,8 @@
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 from wtforms import Form, BooleanField, StringField, PasswordField, validators
 
 class AddPoint(Form):
     name = StringField('Title', [validators.Length(min=4, max=50)])
     coordinates = StringField('Coordinates', [validators.Length(min=0, max=250)])
+    body = TextAreaField('Description', [validators.Length(min=0, max=5000)])
